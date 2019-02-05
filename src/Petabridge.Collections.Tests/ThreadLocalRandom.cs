5849484075de77ec85be897ebb40af0e288ebe20
Aaron Stannard <aaron@petabridge.com>
2019-02-05 15:26:50 -0600

    added FsCheck specs to validate integrity of collections

---
+++ src/Petabridge.Collections.Tests/ThreadLocalRandom.cs
@@ -0,0 +1,28 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ThreadLocalRandom.cs" company="Petabridge, LLC">
+//      Copyright (C) 2015 - 2019 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Threading;
+
+namespace Petabridge.Collections.Tests
+{
+    /// <summary>
+    ///     Create random numbers with Thread-specific seeds.
+    ///     Borrowed form Jon Skeet's brilliant C# in Depth: http://csharpindepth.com/Articles/Chapter12/Random.aspx
+    /// </summary>
+    public static class ThreadLocalRandom
+    {
+        private static int _seed = Environment.TickCount;
+
+        private static readonly ThreadLocal<Random> Rng =
+            new ThreadLocal<Random>(() => new Random(Interlocked.Increment(ref _seed)));
+
+        /// <summary>
+        ///     The current random number seed available to this thread
+        /// </summary>
+        public static Random Current => Rng.Value;
+    }
+}
