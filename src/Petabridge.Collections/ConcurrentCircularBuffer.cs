235df6bb4c08b77ba59f83cd37999e42558955e5
Aaron Stannard <aaron@petabridge.com>
2019-02-05 14:51:44 -0600

    brought over collections from Helios.Util namespace

---
+++ src/Petabridge.Collections/ConcurrentCircularBuffer.cs
@@ -0,0 +1,253 @@
+﻿// Copyright (c) Petabridge <https://petabridge.com/>. All rights reserved.
+// Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
+// See ThirdPartyNotices.txt for references to third party code used inside Helios.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Helios.Util.Collections
+{
+    /// <summary>
+    ///     Concurrent circular buffer implementation, synchronized using a monitor
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ConcurrentCircularBuffer<T> : ICircularBuffer<T>
+    {
+        private bool _full;
+
+        public ConcurrentCircularBuffer(int capacity)
+        {
+            Head = 0;
+            Tail = 0;
+            _buffer = new T[capacity];
+        }
+
+        private int SizeUnsafe => _full ? Capacity : (Tail - Head + Capacity)%Capacity;
+
+        /// <summary>
+        ///     FOR TESTING PURPOSES ONLY
+        /// </summary>
+        internal int Head { get; private set; }
+
+        /// <summary>
+        ///     FOR TESTING PURPOSES ONLY
+        /// </summary>
+        internal int Tail { get; private set; }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public int Capacity => _buffer.Length;
+
+
+        public int Size
+        {
+            get
+            {
+                lock (SyncRoot)
+                {
+                    return SizeUnsafe;
+                }
+            }
+        }
+
+        public T Peek()
+        {
+            lock (SyncRoot)
+            {
+                return _buffer[Head];
+            }
+        }
+
+        public void Enqueue(T obj)
+        {
+            lock (SyncRoot)
+            {
+                UnsafeEnqueue(obj);
+            }
+        }
+
+        public void Enqueue(T[] objs)
+        {
+            //Expand
+            lock (SyncRoot)
+            {
+                foreach (var item in objs)
+                {
+                    UnsafeEnqueue(item);
+                }
+            }
+        }
+
+        public T Dequeue()
+        {
+            lock (SyncRoot)
+            {
+                return UnsafeDequeue();
+            }
+        }
+
+        public IEnumerable<T> Dequeue(int count)
+        {
+            IList<T> returnItems;
+
+            lock (SyncRoot)
+            {
+                var availabileItems = Math.Min(count, SizeUnsafe);
+                returnItems = new List<T>(availabileItems);
+
+                if (availabileItems == 0)
+                    return returnItems;
+
+                for (var i = 0; i < availabileItems; i++)
+                {
+                    returnItems.Add(UnsafeDequeue());
+                }
+            }
+
+            return returnItems;
+        }
+
+        public IEnumerable<T> DequeueAll()
+        {
+            return Dequeue(Size);
+        }
+
+        public void Clear()
+        {
+            lock (SyncRoot)
+            {
+                Head = 0;
+                Tail = 0;
+                _full = false;
+            }
+        }
+
+        public void CopyTo(T[] array)
+        {
+            CopyTo(array, 0);
+        }
+
+        public void CopyTo(T[] array, int index)
+        {
+            CopyTo(array, index, Size);
+        }
+
+        public bool TryAdd(T item)
+        {
+            Enqueue(item);
+            return true;
+        }
+
+        public bool TryTake(out T item)
+        {
+            item = default(T);
+            if (Size == 0) return false;
+            item = Dequeue();
+            return true;
+        }
+
+        public T[] ToArray()
+        {
+            lock (SyncRoot)
+            {
+                var bufferCopy = new T[SizeUnsafe];
+                CopyToUnsafe(bufferCopy, 0, bufferCopy.Length);
+                return bufferCopy;
+            }
+        }
+
+        public void CopyTo(T[] array, int index, int count)
+        {
+            lock (SyncRoot)
+            {
+                CopyToUnsafe(array, index, count);
+            }
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            CopyTo((T[]) array, index);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>) ToArray()).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int Count
+        {
+            get { return Size; }
+        }
+
+        public object SyncRoot { get; } = new object();
+
+        public bool IsSynchronized
+        {
+            get { return true; }
+        }
+
+        private void UnsafeEnqueue(T obj)
+        {
+            _full = _full || Tail + 1 == Capacity; // leave FULL flag on
+            _buffer[Tail] = obj;
+            Tail = (Tail + 1)%Capacity;
+        }
+
+        private T UnsafeDequeue()
+        {
+            _full = false; // full is always false as soon as we dequeue
+            var item = _buffer[Head];
+            Head = (Head + 1)%Capacity;
+            return item;
+        }
+
+        public void Add(T item)
+        {
+            Enqueue(item);
+        }
+
+        public bool Contains(T item)
+        {
+            lock (SyncRoot)
+            {
+                return _buffer.Any(x => x.GetHashCode() == item.GetHashCode());
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            return false;
+        }
+
+        private void CopyToUnsafe(T[] array, int index, int count)
+        {
+            if (count > SizeUnsafe) //The maximum value of count is Size
+                count = SizeUnsafe;
+
+            var bufferBegin = Head;
+            for (var i = 0; i < count; i++, bufferBegin = (bufferBegin + 1)%Capacity, index++)
+            {
+                array[index] = _buffer[bufferBegin];
+            }
+        }
+
+        #region Internal members
+
+        /// <summary>
+        ///     The buffer itself
+        /// </summary>
+        private readonly T[] _buffer;
+
+        #endregion
+    }
+}
---
+++ src/Petabridge.Collections/PriorityQueue.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Helios.Util.Collections
+{
+    public class PriorityQueue<T> : IEnumerable<T>
+    {
+        private readonly IComparer<T> _comparer;
+        private int _capacity;
+        private T[] _items;
+
+        public PriorityQueue(IComparer<T> comparer)
+        {
+            this._comparer = comparer;
+            _capacity = 11;
+            _items = new T[_capacity];
+        }
+
+        public PriorityQueue()
+            : this(Comparer<T>.Default)
+        {
+        }
+
+        public int Count { get; private set; }
+
+        /// <summary>
+        /// TODO: need to make this return the priority queue contents in the correct order
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (var i = 0; i < Count; i++)
+            {
+                yield return _items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public T Dequeue()
+        {
+            var result = Peek();
+            if (result == null)
+            {
+                return default(T);
+            }
+
+            var newCount = --Count;
+            var lastItem = _items[newCount];
+            _items[newCount] = default(T);
+            if (newCount > 0)
+            {
+                TrickleDown(0, lastItem);
+            }
+
+            return result;
+        }
+
+        public T Peek()
+        {
+            return Count == 0 ? default(T) : _items[0];
+        }
+
+        public void Enqueue(T item)
+        {
+            var oldCount = Count;
+            if (oldCount == _capacity)
+            {
+                GrowHeap();
+            }
+            Count = oldCount + 1;
+            BubbleUp(oldCount, item);
+        }
+
+        public void Remove(T item)
+        {
+            var index = Array.IndexOf(_items, item);
+            if (index == -1)
+            {
+                return;
+            }
+
+            Count--;
+            if (index == Count)
+            {
+                _items[index] = default(T);
+            }
+            else
+            {
+                var last = _items[Count];
+                _items[Count] = default(T);
+                TrickleDown(index, last);
+                if (Equals(_items[index], last))
+                {
+                    BubbleUp(index, last);
+                }
+            }
+        }
+
+        private void BubbleUp(int index, T item)
+        {
+            // index > 0 means there is a parent
+            while (index > 0)
+            {
+                var parentIndex = (index - 1) >> 1;
+                var parentItem = _items[parentIndex];
+                if (_comparer.Compare(item, parentItem) >= 0)
+                {
+                    break;
+                }
+                _items[index] = parentItem;
+                index = parentIndex;
+            }
+            _items[index] = item;
+        }
+
+        private void GrowHeap()
+        {
+            var oldCapacity = _capacity;
+            _capacity = oldCapacity + (oldCapacity <= 64 ? oldCapacity + 2 : oldCapacity >> 1);
+            var newHeap = new T[_capacity];
+            Array.Copy(_items, 0, newHeap, 0, Count);
+            _items = newHeap;
+        }
+
+        private void TrickleDown(int index, T item)
+        {
+            var middleIndex = Count >> 1;
+            while (index < middleIndex)
+            {
+                var childIndex = (index << 1) + 1;
+                var childItem = _items[childIndex];
+                var rightChildIndex = childIndex + 1;
+                if (rightChildIndex < Count
+                    && _comparer.Compare(childItem, _items[rightChildIndex]) > 0)
+                {
+                    childIndex = rightChildIndex;
+                    childItem = _items[rightChildIndex];
+                }
+                if (_comparer.Compare(item, childItem) <= 0)
+                {
+                    break;
+                }
+                _items[index] = childItem;
+                index = childIndex;
+            }
+            _items[index] = item;
+        }
+
+        public void Clear()
+        {
+            Count = 0;
+            Array.Clear(_items, 0, 0);
+        }
+    }
+}
